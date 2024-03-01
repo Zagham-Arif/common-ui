0399711fd21814a6813bb76f6ab57e2ffd2fe12e
Zagham-Arif <zaghamarif@gmail.com>
2024-03-01 14:32:57 +0500

    - added build for the repo

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export * from "./src/components/Button";
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/src/components/Button.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+export interface ButtonProps {
+    text?: string;
+}
+export declare const Button: React.FC<ButtonProps>;
+//# sourceMappingURL=Button.d.ts.map
