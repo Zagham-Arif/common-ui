5302250f1bce0664d1fd8185f3a377cc8e0c9dfc
Zagham-Arif <zaghamarif@gmail.com>
2024-03-01 14:20:54 +0500

    - fixes with the configs

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(() => ({
     react(),
     tsConfigPaths(),
     dts({
-      include: ["index.ts"],
+      include: ["src/components", "index.ts"],
     }),
   ],
   resolve: {
